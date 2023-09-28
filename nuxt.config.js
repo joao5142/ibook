f3017d8135e58d8aaac9b70cf82c39ad349f959a
joao5142 <joaopauloneto3687@gmail.com>
2023-09-28 11:00:24 -0300

    refactor:change 404 error page

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,38 +8,42 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Uma nova maneira de ler livros online!' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Uma nova maneira de ler livros online!',
+      },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['normalize.css/normalize.css','@/assets/scss/base.scss'],
+  css: ['normalize.css/normalize.css', '@/assets/scss/base.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: ['@/plugins/accessor'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
-  components: [
-    {path:"@/components",pathPrefix:false}
-  ],
+  components: [{ path: '@/components', pathPrefix: false }],
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
-    '@nuxtjs/style-resources'
+    '@nuxtjs/style-resources',
   ],
-  axios:{
-    baseURL:'https://www.googleapis.com/books/v1'
+  axios: {
+    baseURL: 'https://www.googleapis.com/books/v1',
   },
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: ["@nuxtjs/axios"],
+  modules: ['@nuxtjs/axios'],
 
   styleResources: {
     scss: ['@/components/bosons/*.scss'],
-   },
-  build:{}
-
+  },
+  build: {},
+  generate: {
+    fallback: true,
+  },
 }
